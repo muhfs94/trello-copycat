d5f4e75e15b769e2ad94842fcffaf8f2ee818a91
muh <madmedmod@gmail.com>
2019-08-20 00:27:03 +0700

    initial commit

---
+++ src/components/TrelloBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout
+        row
+        align-center
+        wrap
+      >
+        <v-flex
+          sm3
+          v-for="board in 3"
+          :key="board._id"
+          pa-2
+        >
+          <TrelloBoardList />
+        </v-flex>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+import TrelloBoardList from './TrelloBoardList';
+export default {
+  components: {
+    TrelloBoardList
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin-top: 20px;
+}
+</style>
